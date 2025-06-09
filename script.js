6c33d062bc8609ac734f5b52644c444f68e3db32
unknown <silveriolance06@gmail.com>
2025-06-09 22:03:51 +0800

    Finalized css component

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// INITIALIZE HTML ELEMENTS
+const addButton = document.getElementById('add')
+const delButton = document.getElementById('del')
+const updateButton = document.getElementById('update')
+
 // BOOK OBJECT WITH CONSTRUCTOR
 
 // BOOK ARRAY
 
-// ADD BOOK
+// ADD BOOK, show modal, get input value, add to constructor and add to array
 
-// UPDATE STATUS
+// UPDATE STATUS, show modal and iterate array through dropdown
 
-// DELETE BOOK
+// DELETE BOOK, show modal and iterate array through dropdown
 
-// SHOW BOOK
+// SHOW BOOK, iterate whole array and inject to html element
